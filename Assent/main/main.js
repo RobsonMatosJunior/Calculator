ab7626a68c2a86592734c8d676a47e4cf51c8be3
Robson Junior <111658874+RobsonMatosJunior@users.noreply.github.com>
2023-01-29 15:43:12 -0300

    create of const in js

--- Assent/main/main.js
+++ Assent/main/main.js
@@ -1,3 +1,8 @@
 const botoesNumeros = document.querySelectorAll('[numeros]');
 const botoesOperacoes = document.querySelectorAll('[operacoes]');
-const botaoigual = document.querySelector('[igual]')
+const botaoigual = document.querySelector('[igual]');
+const botaolimpar = document.querySelector('[limpar]');
+const botaoLimparTudo= document.querySelector('[limpar-tudo]');
+const operacao= document.querySelector('[operacao]');
+const posOperacao= document.querySelector('[pos-operacao]');
+
